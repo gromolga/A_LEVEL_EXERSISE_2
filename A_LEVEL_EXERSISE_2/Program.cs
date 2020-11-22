3a356e889e02b7903830e5da4dd8b7662a102a06
Hrom Olha <gromolga.work@gmail.com>
2020-11-22 15:49:15 +0200

    added input and output of arguments

--- A_LEVEL_EXERSISE_2/Program.cs
+++ A_LEVEL_EXERSISE_2/Program.cs
@@ -6,7 +6,22 @@ namespace A_LEVEL_EXERSISE_2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string a;
+            string b;
+            string c;
+            double x1;
+            double x2;
+
+            Console.Write("Enter A value (value must be numeric): ");
+            a = Console.ReadLine();
+
+            Console.Write("Enter B value (value must be numeric): ");
+            b = Console.ReadLine();
+
+            Console.Write("Enter C value (value must be numeric): ");
+            c = Console.ReadLine();
+
+            Console.WriteLine($"a is: {a}; b is: {b}; c is: {c};");
         }
     }
 }
