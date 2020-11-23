f38dd57ad5c7437eb7f199a1913e2e8f841c7ea2
Hrom Olha <gromolga.work@gmail.com>
2020-11-23 09:30:49 +0200

    added posibility to enter again or to escape

--- A_LEVEL_EXERSISE_2/Program.cs
+++ A_LEVEL_EXERSISE_2/Program.cs
@@ -6,47 +6,62 @@ namespace A_LEVEL_EXERSISE_2
     {
         static void Main(string[] args)
         {
-            string a;
-            string b;
-            string c;
-            double x1 = 0;
-            double x2 = 0;
-            double discriminant;
-            double[] results;
+            bool continueValueInput;
+            do {
+                string a;
+                string b;
+                string c;
+                double x1 = 0;
+                double x2 = 0;
+                double discriminant;
+                double[] results;
 
-            Console.WriteLine("Enter a value. Value must be a number");
-            a = Console.ReadLine();
+                Console.WriteLine("Enter a value. Value must be a number");
+                a = Console.ReadLine();
 
-            Console.WriteLine("Enter b value. Value must be a number");
-            b = Console.ReadLine();
+                Console.WriteLine("Enter b value. Value must be a number");
+                b = Console.ReadLine();
 
-            Console.WriteLine("Enter c value. Value must be a number");
-            c = Console.ReadLine();
-       
-            int intA = int.Parse(a);
-            int intB = int.Parse(b);
-            int intC = int.Parse(c);
+                Console.WriteLine("Enter c value. Value must be a number");
+                c = Console.ReadLine();
 
-            discriminant = Math.Pow(intB, 2) - 4 * intA * intC;
+                int intA = int.Parse(a);
+                int intB = int.Parse(b);
+                int intC = int.Parse(c);
 
-            if (discriminant < 0)
-            {
-                Console.Write("The equation hasn't x1, x2");
-            }
-            if (discriminant == 0)
-            {
-                x1 = -intB / (2 * intA);
+                discriminant = Math.Pow(intB, 2) - 4 * intA * intC;
 
-                Console.WriteLine($"The X1 is:  {x1}");
-            }
-            else if (discriminant > 0)
-            {
-                x1 = (-intB + Math.Sqrt(discriminant)) / (2 * intA);
-                x2 = (-intB - Math.Sqrt(discriminant)) / (2 * intA);
+                if (discriminant < 0)
+                {
+                    Console.WriteLine("The equation hasn't x1, x2");
+                }
+                if (discriminant == 0)
+                {
+                    x1 = -intB / (2 * intA);
 
-                Console.WriteLine($"The X1 is:  {x1}");
-                Console.WriteLine($"The X2 is:  {x2}");
-            }
+                    Console.WriteLine($"The X1 is:  {x1}");
+                }
+                else if (discriminant > 0)
+                {
+                    x1 = (-intB + Math.Sqrt(discriminant)) / (2 * intA);
+                    x2 = (-intB - Math.Sqrt(discriminant)) / (2 * intA);
+
+                    Console.WriteLine($"The X1 is:  {x1}");
+                    Console.WriteLine($"The X2 is:  {x2}");
+                }
+
+                Console.WriteLine("If you want to escape - press 1; if you want to continue - press any other key");
+                string userAnswer = Console.ReadLine();
+
+                if(Equals(userAnswer, "1"))
+                {
+                    continueValueInput = true;
+                } else
+                {
+                    continueValueInput = false;
+                }
+
+            } while (continueValueInput);
         }
     }
 }
