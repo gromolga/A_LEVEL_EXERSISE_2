bcab365a749e5d4973978ee109895ae0d073daaf
Hrom Olha <gromolga.work@gmail.com>
2020-11-23 09:43:39 +0200

    added library for calculation logic

--- A_LEVEL_EXERSISE_2/Program.cs
+++ A_LEVEL_EXERSISE_2/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using CalculationLogic;
 
 namespace A_LEVEL_EXERSISE_2
 {
@@ -25,29 +26,22 @@ namespace A_LEVEL_EXERSISE_2
                 Console.WriteLine("Enter c value. Value must be a number");
                 c = Console.ReadLine();
 
-                int intA = int.Parse(a);
-                int intB = int.Parse(b);
-                int intC = int.Parse(c);
+                Calculation calculation = new Calculation();
 
-                discriminant = Math.Pow(intB, 2) - 4 * intA * intC;
+                double disc = calculation.calc(a, b, c, out x1, out x2);
 
-                if (discriminant < 0)
+                if (disc < 0)
                 {
                     Console.WriteLine("The equation hasn't x1, x2");
                 }
-                if (discriminant == 0)
+                else if (disc == 0)
                 {
-                    x1 = -intB / (2 * intA);
-
                     Console.WriteLine($"The X1 is:  {x1}");
                 }
-                else if (discriminant > 0)
+                else
                 {
-                    x1 = (-intB + Math.Sqrt(discriminant)) / (2 * intA);
-                    x2 = (-intB - Math.Sqrt(discriminant)) / (2 * intA);
-
                     Console.WriteLine($"The X1 is:  {x1}");
-                    Console.WriteLine($"The X2 is:  {x2}");
+                    Console.WriteLine($"The X1 is:  {x2}");
                 }
 
                 Console.WriteLine("If you want to escape - press 1; if you want to continue - press any other key");
---
+++ CalculationLogic/Calculation.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace CalculationLogic
+{
+    public class Calculation
+    {
+        public double calc(string a, string b, string c, out double x1, out double x2)
+        {
+            double discriminant;
+            double[] results;
+            x1 = 0;
+            x2 = 0;
+
+            int intA = int.Parse(a);
+            int intB = int.Parse(b);
+            int intC = int.Parse(c);
+
+            discriminant = Math.Pow(intB, 2) - 4 * intA * intC;
+
+
+            if (discriminant == 0)
+            {
+                x1 = -intB / (2 * intA);
+            }
+            else if (discriminant > 0)
+            {
+                x1 = (-intB + Math.Sqrt(discriminant)) / (2 * intA);
+                x2 = (-intB - Math.Sqrt(discriminant)) / (2 * intA);
+            }
+
+            return discriminant;
+        }
+    }
+}
